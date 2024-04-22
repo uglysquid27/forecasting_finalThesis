d178015398b71f7a5c2be4f89dfff31d2b04ffd0
unknown <farrel.ahmad.zaidan@gmail.com>
2024-04-22 22:10:06 +0700

    Update

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ import pandas as pd
 import mysql.connector
 from statsmodels.tsa.arima.model import ARIMA
 import matplotlib.pyplot as plt
+import numpy as np 
 from flask import jsonify
 
 db_config = {
@@ -26,7 +27,7 @@ def get_data_from_table():
         cursor.close()
         conn.close()
 
-        return '\n'.join(str(row) for row in rows)
+        return '\n'.join(str(row) for row in rows) 
 
     except Exception as e:
         return f"Failed to fetch data from the database: {str(e)}"
@@ -44,26 +45,24 @@ def fetch_data_from_database_and_predict():
         cursor.close()
         conn.close()
 
+        if not rows:
+            return jsonify({'error': 'No data found in the database'})
+
         df = pd.DataFrame(rows, columns=['Date', 'Device_Name', 'Value'])  
         df['Date'] = pd.to_datetime(df['Date'])
         df.set_index('Date', inplace=True)
 
-        df.index = df.index.strftime('%Y-%m-%d')
-
-        forecast_steps = 10  
-        model = ARIMA(df['Value'], order=(1, 1, 0)) 
-        results = model.fit()
-        forecast = results.forecast(steps=forecast_steps)
-
-        forecast_values = forecast.tolist()
+        df['Diff'] = df['Value'].diff()  # Calculate the differences
 
         response_data = {
-            'forecast_values': forecast_values,
-            'data': df.to_dict(orient='records')
+            'fetched_data': df.to_dict(orient='records')
         }
 
         return jsonify(response_data)
 
     except Exception as e:
-        print(f"Failed to fetch data from the database or perform ARIMA prediction: {str(e)}")
-        return jsonify({'error': 'Failed to fetch data from the database or perform ARIMA prediction'})
+        print(f"Error: {str(e)}")
+        return jsonify({'error': 'Failed to fetch data from the database'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
